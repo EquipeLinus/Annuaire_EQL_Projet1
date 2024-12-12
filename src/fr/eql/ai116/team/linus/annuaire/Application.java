9d99e5f5dd869a453a9d368adf8674277fc5978a
thomas <a@b.c>
2024-12-12 17:20:51 +0100

    premiers tests d'architecture

--- src/fr/eql/ai116/team/linus/annuaire/Application.java
+++ src/fr/eql/ai116/team/linus/annuaire/Application.java
@@ -1,12 +1,21 @@
 package fr.eql.ai116.team.linus.annuaire;
 
+import fr.eql.ai116.team.linus.annuaire.model.entity.Stagiaire;
+import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Application {
+import java.util.List;
+
+public class Application extends javafx.application.Application {
 
     private static final Logger log = LogManager.getLogger();
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
     }
 }
