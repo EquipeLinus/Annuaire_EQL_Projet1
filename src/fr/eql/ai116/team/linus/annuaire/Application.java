b79738bb74f03a7d4e0e29a750de606c213aa670
thomas <a@b.c>
2024-12-12 16:37:25 +0100

    Logger & Application.java

---
+++ src/fr/eql/ai116/team/linus/annuaire/Application.java
@@ -0,0 +1,12 @@
+package fr.eql.ai116.team.linus.annuaire;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Application {
+
+    private static final Logger log = LogManager.getLogger();
+    public static void main(String[] args) {
+
+    }
+}
