de5c0d48a3e98fb50783e62274e03ca24493f5ee
Andras <andrasschuller@gmail.com>
2024-12-18 14:01:41 +0100

    commit for merge

--- src/fr/eql/ai116/team/linus/annuaire/view/elements/PromoStack.java
+++ src/fr/eql/ai116/team/linus/annuaire/view/elements/PromoStack.java
@@ -1,5 +1,6 @@
-package fr.eql.ai116.team.linus.annuaire.view;
+package fr.eql.ai116.team.linus.annuaire.view.elements;
 
+import fr.eql.ai116.team.linus.annuaire.view.Clean;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
@@ -9,13 +10,13 @@ import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
 
 public class PromoStack extends HBox {
-
     private HBox buttonContainer = new HBox();
     private final TextField promoTxt;
     private List<String> selectedPromos = new ArrayList<>();
+    private List<Button> selectedButtons = new ArrayList<>();
+
 
     public PromoStack(TextField promoTxt) {
         super(5.);
@@ -31,53 +32,91 @@ public class PromoStack extends HBox {
         btnValidate.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                addPromo(promoTxt.getText());
-            }
+                promoButtons();}
         });
     }
 
-    public void addPromos (String name) {
-        if (name.isEmpty()) return;
-        String cleanedName = Clean.cleanPromo(name);
-        promoTxt.setText("");
-        Button promoBtn = new Button(cleanedName);
-        promoBtn.setMinWidth(80);
-        promoBtn.setMaxWidth(80);
+    private void promoButtons() {
+        addPromo(promoTxt.getText());
+        displayPromoButtons ();
     }
 
-    public void addPromo(String name) {
-        if (name.isEmpty()) return;
-        String cleanedName = Clean.cleanPromo(name);
-        promoTxt.setText("");
-        Button promoBtn = new Button(cleanedName);
-        promoBtn.setMinWidth(80);
-        promoBtn.setMaxWidth(80);
+    private void displayPromoButtons() {
+        selectedPromos.sort(String::compareTo); // A verifier si la méthode sauvegarde la liste ordonnée dans la liste originale...
+        for (String selectedPromo : selectedPromos) {
+            Button promoBtn = new Button (selectedPromo);
+            promoBtn.setMinWidth(80);
+            promoBtn.setMaxWidth(80);
 
-        promoBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                removePromo(cleanedName);
-            }
-        });
-
-        buttonContainer.getChildren().add(promoBtn);
-    }
-
-    public void removePromo(String name) {
-
-        for (Node child : buttonContainer.getChildren()) {
-            if (child instanceof Button && ((Button) child).getText().equals(name)) {
-                buttonContainer.getChildren().remove(child);
-                break;
-            }
+            promoBtn.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    removePromo(selectedPromo);
+                }
+            });
+            buttonContainer.getChildren().addAll(selectedButtons);
         }
     }
 
-    public String[] getValidatedPromos() {
-        String[] result = new String[buttonContainer.getChildren().size()];
-        for (int i = 0; i < buttonContainer.getChildren().size(); i++) {
-            result[i] = ((Button)buttonContainer.getChildren().get(i)).getText();
+    public void addPromos(String name) {
+        if (name.isEmpty()) return;
+        promoTxt.setText(""); // A réflechir où à le mettre...
+        String cleanedName = Clean.cleanPromo(name);
+        selectedPromos.add(cleanedName);
+        selectedPromos.sort(String::compareTo); // A verifier si la méthode sauvegarde la liste ordonnée dans la liste originale...
+        for (String selectedPromo : selectedPromos) {
+            Button promoBtn = new Button(cleanedName);
+            promoBtn.setMinWidth(80);
+            promoBtn.setMaxWidth(80);
+            selectedButtons.add(promoBtn);
+
+            promoBtn.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    removePromo(cleanedName);
+                }
+            });
+            buttonContainer.getChildren().addAll(selectedButtons);
         }
-        return result;
     }
+
+        public void addPromo (String name){
+            if (name.isEmpty()) return;
+            String cleanedName = Clean.cleanPromo(name);
+            selectedPromos.add(cleanedName);
+            promoTxt.setText("");
+    }
+
+            /*
+            Button promoBtn = new Button(cleanedName);
+            promoBtn.setMinWidth(80);
+            promoBtn.setMaxWidth(80);
+
+            promoBtn.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    removePromo(cleanedName);
+                }
+            });
+
+            buttonContainer.getChildren().add(promoBtn);
+        }*/
+
+        public void removePromo (String name){
+
+            for (Node child : buttonContainer.getChildren()) {
+                if (child instanceof Button && ((Button) child).getText().equals(name)) {
+                    buttonContainer.getChildren().remove(child);
+                    break;
+                }
+            }
+        }
+
+        public String[] getValidatedPromos () {
+            String[] result = new String[buttonContainer.getChildren().size()];
+            for (int i = 0; i < buttonContainer.getChildren().size(); i++) {
+                result[i] = ((Button) buttonContainer.getChildren().get(i)).getText();
+            }
+            return result;
+        }
 }
